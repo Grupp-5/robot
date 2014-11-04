19bc7b1ef99f71b5cd65f5b696af39e5db3ff993
Emil <emil.vagstedt@gmail.com>
2014-11-04 09:37:26 +0100

    initial commit

---
+++ robot/robot.c
@@ -0,0 +1,17 @@
+/*
+ * robot.c
+ *
+ * Created: 11/4/2014 9:35:08 AM
+ *  Author: emiva760
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
