136b4e04a14193dae2efe42d1f0031a7549956a3
Daniel Sjövall <dsjovall@gmail.com>
2014-11-04 11:36:21 +0100

    Börjat plocka lite med UART

---
+++ robot/styrenhet/styrenhet.c
@@ -0,0 +1,25 @@
+/*
+* Styrenheten
+* Date : 2014.11.04
+* Author : Daniel
+*/
+
+#include "constants.c"
+
+
+void uart_putchar(char c){
+	loop_until_bit_is_set(UCSR0A, UDRE0); //Vänta tills dataregistret är tomt, RÄTT REGISTER???
+	UDR0 = c;
+}
+
+char uart_getchar(void) {
+    loop_until_bit_is_set(UCSR0A, RXC0); /* Vänta tills data existerar, RÄTT REGISTER???. */
+    return UDR0;
+}
+
+void PortInitialize(void){
+	DDRA = DDRB = DDRC = DDRD = 0; //Sätt alla portar till input
+	PORTA = PORTB = PORTC = PORTD = 0; //Sätt portdata till 0
+
+	DDRD = 0x40; //Pin D1 output, resten input
+}
