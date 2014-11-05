272ea0e53ea1e433ee22961275b149ed09a8e40c
Daniel Sjövall <dsjovall@gmail.com>
2014-11-05 12:39:59 +0100

    Förhoppningsvis någon nästan fungerande kommunikation med servona via UART

--- robot/styrenhet/ax12.c
+++ robot/styrenhet/ax12.c
@@ -4,12 +4,24 @@
 * Author : Daniel
 */
 
-#include "constants.c"
+#include "ax12.h"
+
+//Globala variabler
+byte checksum;
+
+//Initiera uart
+void uart_init (void) {
+	UBRRH = (DEFAULT_BAUD_RATE>>8); //Skifta registret höger 8 bitar
+	UBRRL = DEFAULT_BAUD_RATE; //Sätt baud rate
+	UCSRB|= (1<<TXEN)|(1<<RXEN); //Aktivera sändare och mottagare
+	UCSRC|= (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1); //Sätt til 8bits instruktioner
+}
 
 
 void uart_putchar(char c) {
 	loop_until_bit_is_set(UCSR0A, UDRE0); //Vänta tills dataregistret är tomt, RÄTT REGISTER???
 	UDR0 = c;
+	checksum += c;
 }
 
 char uart_getchar(void) {
@@ -24,27 +36,37 @@ void PortInitialize(void) {
 	DDRD = 0x40; //Pin D1 output, resten input
 }
 
+//Initiera uart
+void uart_init (void) {
+	UBRRH = (DEFAULT_BAUD_RATE>>8); //Skifta registret höger 8 bitar
+	UBRRL = DEFAULT_BAUD_RATE; //Sätt baud rate
+	UCSRB|= (1<<TXEN)|(1<<RXEN); //Aktivera sändare och mottagare
+	UCSRC|= (1<<URSEL)|(1<<UCSZ0)|(1<<UCSZ1); //Sätt til 8bits instruktioner
+}
+
 // Funktioner för att skicka och ta emot paket
 
 void PushHeaderAX(byte id, byte len, byte inst) {
 	//Pusha information till uart
-	//FF
-	//FF
-	//ID
-	//LENGTH
-	//INST
-	//PARAM1
-	//PARAM2
+	uart_putchar(0xFF);
+	uart_putchar(0xFF);
+
+	checksum = 0; //Räkna inte med de första två bitarna
+
+	uart_putchar(id);
+	uart_putchar(len + 2);
+	uart_putchar(inst);
 }
 
 void PushFooterAX() {
 	//CHECKSUM
+	uart_putchar(~checksum);
 }
 
 
 // Instruktioner
 
 void PingAX(byte id) {
-	//skicka header
-	//skicka footer
+	PushHeaderAX(id, 2, AX_INST_PING); //skicka header
+	PushFooterAX(); //skicka footer
 }
