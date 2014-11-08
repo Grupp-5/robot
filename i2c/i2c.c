1d5d2df431149356f845444bbe69fce262c2c5c0
emilknievel <emil.vagstedt@gmail.com>
2014-11-08 19:26:40 +0100

    Kodskal till i2c

---
+++ i2c/i2c.c
@@ -0,0 +1,18 @@
+/*
+ * i2c.c
+ *
+ * Created: 2014-11-08 12:38:10
+ *  Author: Emil
+ */ 
+
+
+#include <avr/io.h>
+#include <util/twi.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
