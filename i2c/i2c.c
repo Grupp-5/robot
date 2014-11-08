15f9a365eda9c9335b9cde3d545b2c6e94ae857c
emilknievel <emil.vagstedt@gmail.com>
2014-11-08 22:53:02 +0100

    Påbörjad i2c-kod. Går inte att använda till nåt än

--- i2c/i2c.c
+++ i2c/i2c.c
@@ -2,12 +2,35 @@
  * i2c.c
  *
  * Created: 2014-11-08 12:38:10
- *  Author: Emil
+ *  Author: emiva760, carth567
  */ 
 
 
 #include <avr/io.h>
 #include <util/twi.h>
+#include "i2c.h"
+
+// Skicka data till I2C-enhet
+unsigned char i2c_transmit(unsigned char type) {
+	switch(type) {
+	case I2C_START:	// Skicka start condition
+		TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+		break;
+	case I2C_DATA:	// Skicka data
+		TWCR = (1 << TWINT) | (1 << TWEN);
+		break;
+	case I2C_STOP:	// Skicka stop condition
+		TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+		return 0;
+	}
+	
+	// Vänta på att TWINT flaggan sätts i TWCR-register
+	while(!(TWCR & (1 << TWINT)))
+		;
+	
+	// Returnera TWI statusregister, maskera prescaler bitar (??)
+	return (TWSR & TW_NO_INFO);
+}
 
 int main(void)
 {
