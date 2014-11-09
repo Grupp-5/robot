1aa07eeaf07930443f816f174f863826bf529276
Daniel Sjövall <dsjovall@gmail.com>
2014-11-09 19:27:11 +0100

    Lagt till liggpostion

--- styrenhet/servo.c
+++ styrenhet/servo.c
@@ -21,6 +21,39 @@ uint16_t FixMirroring(byte id, uint16_t value) {
 	return value; 
 }
 
-void SetStartPosition () {
+void SendCoxa(uint16_t value) {
+	SetPositionAX(1, FixMirroring(1, value));
+	SetPositionAX(13, FixMirroring(13, value));
+	SetPositionAX(7, FixMirroring(7, value));
+	SetPositionAX(8, FixMirroring(8, value));
+	SetPositionAX(14, FixMirroring(14, value));
+	SetPositionAX(2, FixMirroring(2, value));
+}
 
+void SendFemur(uint16_t value) {
+	SetPositionAX(3, FixMirroring(3, value));
+	SetPositionAX(15, FixMirroring(15, value));
+	SetPositionAX(9, FixMirroring(9, value));
+	SetPositionAX(10, FixMirroring(10, value));
+	SetPositionAX(16, FixMirroring(16, value));
+	SetPositionAX(4, FixMirroring(4, value));
+}
+
+void SendTibia(uint16_t value) {
+	SetPositionAX(5, FixMirroring(5, value));
+	SetPositionAX(17, FixMirroring(17, value));
+	SetPositionAX(11, FixMirroring(11, value));
+	SetPositionAX(12, FixMirroring(12, value));
+	SetPositionAX(18, FixMirroring(18, value));
+	SetPositionAX(6, FixMirroring(6, value));
+}
+
+void SetLayPosition() {
+	SendCoxa(511);
+	SendFemur(200);
+	SendTibia(777);
+}
+
+void SetStartPosition() {	
+	SendFemur(???);
 }
