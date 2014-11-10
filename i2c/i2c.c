22bb56bc84ce5719027aa1d6a7934757a921c6a8
Emil <emil.vagstedt@gmail.com>
2014-11-10 11:42:09 +0100

    Lagt till lite läskod till i2c

--- i2c/i2c.c
+++ i2c/i2c.c
@@ -89,11 +89,90 @@ int i2c_writebyte(	unsigned int i2c_address, unsigned int dev_id,
 	// TWI-överföring OK
 	r_val = 1;
 	
-	i2_quit:
+	i2c_quit:
 	// Överför i2c_data
 	twi_status = i2c_transmit(I2C_STOP);
 	return r_val;
 }
+
+int i2c_readbyte(	unsigned int i2c_address, unsigned int dev_id,
+					unsigned int dev_addr, char *data ) {
+	unsigned char n = 0;
+	unsigned char twi_status;
+	char r_val = -1;
+	
+	i2c_retry:
+	if(n++ >= MAX_TRIES) return r_val;
+	
+	twi_status = i2c_transmit(I2C_START); // Överför start condition
+	
+	// Kolla TWSR-status
+	if(twi_status == TW_MT_ARB_LOST) goto i2c_retry;
+	if((twi_status != TW_START) && (twi_status != TW_REP_START)) goto i2c_quit; 
+	
+	// Skicka slav-adress (SLA_W)
+	TWDR = (dev_id & 0xF0) | ((dev_addr << 1) && 0x0E) | TW_WRITE;
+	
+	// Överför i2c-data
+	twi_status = i2c_transmit(I2C_DATA);
+	
+	// Kolla TWSR-status
+	if((twi_status == TW_MT_SLA_NACK) || (twi_status == TW_MT_ARB_LOST))
+		goto i2c_retry;
+	if(twi_status != TW_MT_SLA_ACK) goto i2c_quit;
+	
+	// Skicka 8 MSB av i2c-adress
+	TWDR = i2c_address >> 8;
+	
+	// Överför i2c-data
+	twi_status = i2c_transmit(I2C_DATA);
+	
+	// Kolla TWSR-status
+	if(twi_status != TW_MT_DATA_ACK) goto i2c_quit;
+	
+	// Skicka 8 LSB av i2c-adress
+	TWDR = i2c_address;
+	
+	// Överför i2c-data
+	twi_status = i2c_transmit(I2C_DATA);
+	
+	// Kolla TWSR-status
+	if(twi_status != TW_MT_DATA_ACK) goto i2c_quit;
+	
+	// Skicka start condition
+	twi_status = i2c_transmit(I2C_START);
+	
+	// Kolla TWSR-status
+	if(twi_status == TW_MT_ARB_LOST) goto i2c_retry;
+	if((twi_status != TW_START) && (twi_status != TW_REP_START))
+		goto i2c_quit;
+		
+	// Skicka slav-adress (SLA_R)
+	TWDR = (dev_id & 0xF0) | ((dev_addr << 1) & 0x0E) | TW_READ;
+	
+	// Överför i2c-data
+	twi_status = i2c_transmit(I2C_DATA);
+	
+	// Kolla TWSR-status
+	if((twi_status == TW_MR_SLA_NACK) || (twi_status == TW_MR_ARB_LOST))
+		goto i2c_retry;
+	if(twi_status != TW_MR_SLA_ACK) goto i2c_quit;
+	
+	// Läs i2c-data
+	twi_status = i2c_transmit(I2C_DATA);
+	if(twi_status != TW_MR_SLA_NACK) goto i2c_quit;
+	
+	// Hämta datan
+	*data = TWDR;
+	r_val = 1;
+	
+	i2c_quit:
+	// Skicka stop condition
+	twi_status = i2c_transmit(I2C_STOP);
+	return r_val;
+ }
+
+
 
 int main(void)
 {
