afbdfc5630ec1aee3a8944d864bff8212bca2c5d
Emil <emil.vagstedt@gmail.com>
2014-11-11 12:02:09 +0100

    Lagt till i2c-kod från Atmels hemsida. Vi hade nog missförstått lite.

--- i2c/i2c.h
+++ i2c/i2c.h
@@ -1,35 +1,16 @@
-/*
+ï»¿/*
  * i2c.h
  *
- * Created: 2014-11-08 22:01:05
- *  Author: emiva760, carth567
+ * Created: 11/11/2014 11:37:06 AM
+ *  Author: emiva760
  */ 
 
 
 #ifndef I2C_H_
 #define I2C_H_
 
-// Typ av sändning i i2c_transmit
-#define I2C_START 0
-#define I2C_DATA  1
-#define I2C_STOP  2
-#define MAX_TRIES 50
 
-enum Device {
-	CONTROL,
-	COMMUNICATION,
-	SENSOR,
-	DECISION
-};
 
-unsigned char i2c_transmit(unsigned char type);
-int i2c_writebyte(
-					unsigned int i2c_address, unsigned int dev_id,
-					unsigned int dev_addr, char data
-				 );
-int i2c_readbyte(
-					unsigned int i2c_address, unsigned int dev_id,
-					unsigned int dev_addr, char *data
-				);
+
 
 #endif /* I2C_H_ */
