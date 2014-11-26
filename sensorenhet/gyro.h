a4f4c33de7775ddac29f82f8398b47e3d5418725
Emil <emil.vagstedt@gmail.com>
2014-11-26 20:36:23 +0100

    Gyro fungerar (typ lite kanske) i ett testfall och skickar ut ok vinkel för en viss spänning

---
+++ sensorenhet/gyro.h
@@ -0,0 +1,17 @@
+﻿/*
+ * gyro.h
+ *
+ * Created: 11/25/2014 1:48:54 PM
+ *  Author: emiva760, carth567
+ */
+
+
+#ifndef GYRO_H_
+#define GYRO_H_
+
+void SPI_init(void);
+void activate_adc(void);
+void deactivate_adc(void);
+uint16_t read_ar_data(void);
+
+#endif /* GYRO_H_ */
