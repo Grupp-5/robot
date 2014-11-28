67eb07f3ee5cc82485b89093de716ceca2abf26e
Erik Malmberg <erima694@student.liu.se>
2014-11-28 11:02:46 +0100

    Tested som variations of the decisionmaking
    *The robot can detected and react to 90 degree turns
    in both directions, and 3-way crossings with dead ends.
    
    -We need the gyro to make more accurate turns

--- beslutsenhet/beslutsenhet.c
+++ beslutsenhet/beslutsenhet.c
@@ -116,56 +116,64 @@ void makeDecision(void) {
 		sensor_data = (Sensor_data)master_data_to_receive;
 	}
 	
-	if(sensor_data.f < 30)
-	{
-		send_move_data(0, 0, 0);//stop
-		TIMSK1 &= ~(1<<TOIE1);//Disable timer overflow interrupt for Timer1
-		TIMSK3 &= ~(1<<TOIE3);//Disable timer overflow interrupt for Timer3
-		pdFlag  = 0;
-		makeDecisionFlag = 0;
-	}
-	
-	//if(sensor_data.fr<150) {
-		//if(sensor_data.fl<150) {
-//
-			//send_move_data(0.5, 0, 0);//go forward
-			//
-		//}else {
-			//if(sensor_data.f<30) {
-				//send_move_data(0, 0, -0.5);//turn left
-				////Wait for 90 degree turn, by asking gyro
-				//send_move_data(0.5, 0, 0);//go forward
-			//}else {
-				//if(sensor_data.fr<80) {
-					//send_move_data(0, 0, -0.5);//turn left
-					////Wait for 90 degree turn, by asking gyro
-					//send_move_data(0.5, 0, 0);//go forward
-				//}
-			//}
-		//}
-	//}else {
-		//if(sensor_data.f<30) {
-			//send_move_data(0, 0, 0.5);//turn right
-			////Wait for 90 degree turn, by asking gyro
-			//send_move_data(0.5, 0, 0);//go forward
-		//}else {
-			//if(sensor_data.fl<80) {
-				//send_move_data(0, 0, 0.5);//turn right
-				////Wait for 90 degree turn, by asking gyro
-				//send_move_data(0.5, 0, 0);//go forward
-			//}else {
-				//Bus_data stop;
-				//stop.id = STOP_TIMER;
-				//stop.count =  command_lengths[STOP_TIMER];
-				//send_data(which_device[STOP_TIMER], stop);//celebrate
-				//send_move_data(0, 0, 0);//stop
-				//
-				//autoMode = 0;
-				//TIMSK1 &= ~(1<<TOIE1);//Disable timer overflow interrupt for Timer1
-				//TIMSK3 &= ~(1<<TOIE3);//Disable timer overflow interrupt for Timer3
-			//}
-		//}
+	//if(sensor_data.f < 30)
+	//{
+		//send_move_data(0, 0, 0);//stop
+		//TIMSK1 &= ~(1<<TOIE1);//Disable timer overflow interrupt for Timer1
+		//TIMSK3 &= ~(1<<TOIE3);//Disable timer overflow interrupt for Timer3
+		//pdFlag  = 0;
+		//makeDecisionFlag = 0;
 	//}
+	
+	if(sensor_data.fr<150) {
+		if(sensor_data.fl<150) {
+
+			//send_move_data(0.5, 0, 0);//go forward
+			
+		}else {
+			if(sensor_data.f<30) {
+				send_move_data(0.4, 0, -0.8);//turn left
+				_delay_ms(2200);
+				//Wait for 90 degree turn, by asking gyro
+				send_move_data(0.5, 0, 0);//go forward
+				_delay_ms(2500);
+			}else {
+				if(sensor_data.fr<80) {
+					send_move_data(0.4, 0, -0.8);//turn left
+					_delay_ms(2200);
+					//Wait for 90 degree turn, by asking gyro
+					send_move_data(0.5, 0, 0);//go forward
+					_delay_ms(2500);
+				}
+			}
+		}
+	}else {
+		if(sensor_data.f<30) {
+			send_move_data(0.4, 0, 0.8);//turn right
+			_delay_ms(2200);
+			//Wait for 90 degree turn, by asking gyro
+			send_move_data(0.5, 0, 0);//go forward
+			_delay_ms(2500);
+		}else {
+			if(sensor_data.fl<80) {
+				send_move_data(0.4, 0, 0.8);//turn right
+				_delay_ms(2200);
+				//Wait for 90 degree turn, by asking gyro
+				send_move_data(0.5, 0, 0);//go forward
+				_delay_ms(2500);
+			}else {
+				Bus_data stop;
+				stop.id = STOP_TIMER;
+				stop.count =  command_lengths[STOP_TIMER];
+				send_data(which_device[STOP_TIMER], stop);//celebrate
+				send_move_data(0, 0, 0);//stop
+				
+				autoMode = 0;
+				TIMSK1 &= ~(1<<TOIE1);//Disable timer overflow interrupt for Timer1
+				TIMSK3 &= ~(1<<TOIE3);//Disable timer overflow interrupt for Timer3
+			}
+		}
+	}
 }
 
 ISR(TIMER1_OVF_vect) {
